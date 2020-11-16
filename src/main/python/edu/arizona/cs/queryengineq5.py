cb64def6f0443c175f5011f1fa3b966a08dfd7f0
mithun <mithunpaul08@gmail.com>
2020-11-15 20:58:39 -0700

    done with test cases. ready for mihai review

---
+++ src/main/python/edu/arizona/cs/queryengineq5.py
@@ -0,0 +1,24 @@
+
+
+class QueryEngineQ5:
+
+    def __init__(self):
+        # add your code here
+        pass
+
+    def read_txt_file(self):
+        #add your code here
+        docs=None
+        return docs
+
+    def buildmodel(self):
+        # add your code here
+        pass
+
+    #This method should return the F1 score of the classifier when run over test partition
+    def runQ5_2_f1score(self):
+        # dummy code. add your code here
+        f1 = 0.5
+        return f1
+
+
