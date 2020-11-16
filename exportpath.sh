b35ef93783d86313def9274c2e6ec9e1520508d4
Mithun Paul <mithunpaul08@gmail.com>
2020-11-15 22:08:28 -0700

    Update exportpath.sh

--- exportpath.sh
+++ exportpath.sh
@@ -1,1 +1,2 @@
-export PYTHONPATH="/Users/mordor/tawork/fall2020cs4583/hw2/hw2_python/"
+# When you run `pytest` if you get an error saying `src not found`, that is probably because the environment variable PYTHONPATH is not set. Make it point to the folder containing src like this:
+export PYTHONPATH="/Users/mordor/fall2020cs4583/hw2/hw2_python/"
